1cadc01750b621dc537ca22cb30bf8c1ed43f2d1
dieuwbak <dieuwertjeddab@hotmail.nl>
2018-03-12 17:57:30 +0100

    Edited the Tweenmax

--- extra.js
+++ extra.js
@@ -1,19 +1,19 @@
 var timeline = new TimelineMax({repeat: -1});
 
 function startTime() {
-	    var today = new Date();
-	    var h = today.getHours();
-	    var m = today.getMinutes();
-	    var s = today.getSeconds();
-	    m = checkTime(m);
-	    s = checkTime(s);
-	    document.getElementById('clock').innerHTML =
-	    h + ":" + m + ":" + s;
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =
+    h + ":" + m + ":" + s;
 }
 
 function checkTime(i) {
-    	if (i < 10) {i = "0" + i};  
-    	return i;
+	if (i < 10) {i = "0" + i};  
+	return i;
 }
 
 
@@ -21,5 +21,21 @@ function checkTime(i) {
 var animation = document.getElementById('clock');	
 
 timeline.call(startTime)
-		.to(animation, 0.5, {fontSize: '3em'})
-		.from(animation, 0.5, {fontSize: '2.7em'});
+		.call(changeBackground)
+		.to(animation, 0.7, {fontSize: '20vw', ease: Bounce.easeIn})
+		.from(animation, 0.3, {fontSize: '19vw', ease: Sine.easeIn});
+
+
+function changeBackground() {
+	var today = new Date();
+    var hour = today.getHours();
+
+	console.log(hour);
+
+    if (hour <= 15) {
+    	document.body.style.backgroundColor = 'red';
+    }
+    else {
+    	document.body.style.backgroundColor = 'green';
+    }
+}
